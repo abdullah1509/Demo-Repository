643363fcd4b39cf37189cde5364471f320ee5992
Abi <58501537+abdullah1509@users.noreply.github.com>
2023-06-17 23:49:14 +0530

    Create square.py

---
+++ Python/square.py
@@ -0,0 +1,21 @@
+def mySqrt(x):
+    if x == 0:
+        return 0
+
+    left, right = 1, x
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if mid * mid == x:
+            return mid
+        elif mid * mid > x:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return right
+
+x = 8
+print(mySqrt(x))
+
